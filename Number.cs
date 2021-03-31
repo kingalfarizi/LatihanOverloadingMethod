810994c9ddf3f479394ea3acc11627c9694db9dd
kingalfarizi <alfarizidito@gmail.com>
2021-03-31 23:02:01 +0700

    Overloading Method

---
+++ Number.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadingMethod
+{
+    class Number
+    {
+        public int FindMinimum(int number1, int number2)
+        {
+            return Math.Min(number1, number2);
+        }
+        public int FindMinimum(int number1, int number2, int number3)
+        {
+            int IntNumber3 = (int)number3;
+            return Math.Min(number1, Math.Min(number2, IntNumber3));
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            return Math.Max(number1, number2);
+        }
+
+        public int FindMaximum(int number1, int number2, int number3)
+        {
+            int IntNumber3 = (int)number3;
+            return Math.Max(number1, Math.Max(number2, IntNumber3));
+        }
+    }
+}
